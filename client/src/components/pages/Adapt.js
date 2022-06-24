e60f6556ac6a34afcd2295ec6e51c592154ea49c
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-24 13:00:02 +0100

    Ajout des icons

--- client/src/components/pages/Adapt.js
+++ client/src/components/pages/Adapt.js
@@ -2,13 +2,9 @@ import React, { useEffect } from 'react';
 
 const Adapt = () => {
     const vibrate = ()=>{
-        console.log('tet')
-        window.navigator.vibrate([1000,300,1000])
+        window.navigator.vibrate([700,300,700])
     }
     
-    useEffect(()=>{
-        window.navigator.vibrate([1000,300,1000])
-    })
     return (
         <div>       
             <button onClick={vibrate}>test</button>
---
+++ client/src/components/transportButton/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const TransportBtn = (props) => {
+    const [transport, setTransport] = useState(props.transport)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TransportBtn;

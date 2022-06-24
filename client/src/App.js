53d78c437937680eca4c896dceade5c64b03cfb8
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-24 08:36:21 +0100

    Init

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 // import './App.css';
+import Route from './components/routes'
 
 function App() {
   return (
     <div className="App">
-      
+        <Route/>
     </div>
   );
 }
---
+++ client/src/components/pages/Adapt.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Adapt = () => {
+    return (
+        <div>
+            Cette est page est la page des personnes avec un soucis de vue
+        </div>
+    );
+};
+
+export default Adapt;

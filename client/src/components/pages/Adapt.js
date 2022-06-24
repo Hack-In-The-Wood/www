4f523c2deb7bff3a7f89a767ee02266b612ab3cd
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-24 09:19:01 +0100

    Update Adapt.js

--- client/src/components/pages/Adapt.js
+++ client/src/components/pages/Adapt.js
@@ -1,17 +1,14 @@
 import React, { useEffect } from 'react';
 
 const Adapt = () => {
-    const isMobile = ()=>{
-        const nav = navigator.userAgent.toLowerCase();
-        return (
-            nav.match(/iphone/i) || nav.match(/ipod/i) || nav.match(/ipad/i) || nav.match(/android/i)
-        );
+    const vibrate = ()=>{
+        window.navigator.vibrate(1000)
     }
     
     return (
         <div>
-            {isMobile() && window.navigator.vibrate(1000)}
-            Cette est page est la page des personnes avec un soucis de vue
+            
+            <button onClick={vibrate}>test</button>
         </div>
     );
 };
